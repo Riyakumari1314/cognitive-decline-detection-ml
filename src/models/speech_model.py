99ac36fb517c088ae40afcd67a323e4a00e99b51
riya kumari <riyakumari198471@gmail.com>
2026-09-22 20:23:48 +0530

    Add speech classification model and training function
    Initial implementation of speech classification model with a training function.

---
+++ src/models/speech_model.py
@@ -0,0 +1,23 @@
+"""
+Speech classification model.
+
+This module will contain machine-learning models
+for classification using speech features.
+
+Class labels:
+0 = Healthy Control
+1 = Cognitive Decline
+"""
+
+def train_speech_model(X, y):
+    """
+    Train and evaluate a speech-based model.
+
+    The complete implementation will be added after
+    the benchmark dataset is connected.
+    """
+
+    model = None
+    results = {}
+
+    return model, results
