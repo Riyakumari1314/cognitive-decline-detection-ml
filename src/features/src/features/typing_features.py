19aeac7c08273b276a100da6c028eb730bec83b9
riya kumari <riyakumari198471@gmail.com>
2026-09-22 19:54:00 +0530

    Add typing feature extraction module
    Added a module for extracting typing features related to cognitive decline analysis.

---
+++ src/features/src/features/typing_features.py
@@ -0,0 +1,27 @@
+"""
+Typing feature extraction module.
+
+This module will contain functions for extracting
+keystroke and typing-related features relevant to
+cognitive decline analysis.
+"""
+
+def extract_typing_features(typing_data):
+    """
+    Extract features from processed typing data.
+
+    Planned features:
+    - Typing speed
+    - Mean inter-key interval
+    - Pause frequency
+    - Mean pause duration
+    - Backspace frequency
+    - Correction rate
+    - Error rate
+    - Session duration
+    """
+    features = {}
+
+    # Feature extraction will be implemented later.
+
+    return features
