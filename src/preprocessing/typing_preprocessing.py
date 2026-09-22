ac173cff5b8b1070c57b6a0642f68453bdab4e06
riya kumari <riyakumari198471@gmail.com>
2026-09-22 19:52:04 +0530

    Add typing preprocessing module
    This module will contain preprocessing functions for typing and keystroke data used in the cognitive decline detection project.

---
+++ src/preprocessing/typing_preprocessing.py
@@ -0,0 +1,13 @@
+"""
+Typing preprocessing module.
+
+This module will contain preprocessing functions for
+typing and keystroke data used in the cognitive
+decline detection project.
+"""
+
+def preprocess_typing():
+    """
+    Placeholder for the typing preprocessing pipeline.
+    """
+    pass
