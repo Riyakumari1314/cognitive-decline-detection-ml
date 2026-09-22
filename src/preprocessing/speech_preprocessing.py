54a20401ec6434c8345a32f3c7eb0f7e7dcb8016
riya kumari <riyakumari198471@gmail.com>
2026-09-22 19:51:21 +0530

    Add speech preprocessing module
    Initial implementation of the speech preprocessing module with a placeholder function.

---
+++ src/preprocessing/speech_preprocessing.py
@@ -0,0 +1,12 @@
+"""
+Speech preprocessing module.
+
+This module will contain preprocessing functions for
+speech data used in the cognitive decline detection project.
+"""
+
+def preprocess_speech():
+    """
+    Placeholder for the speech preprocessing pipeline.
+    """
+    pass
